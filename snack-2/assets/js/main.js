8eca9b73f1b623bccc04d99468984b6c1bdf6e0f
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-24 12:48:59 +0100

    start commit

---
+++ snack-2/assets/js/main.js
@@ -0,0 +1,17 @@
+//Scrivi una funzione che accetti tre argomenti:
+//un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
+//La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a" e "b"
+
+let a, b, rest;
+[a, b] = [10, 50];
+
+console.log(a);
+// Expected output: 10
+
+console.log(b);
+// Expected output: 20
+
+[a, b, ...rest ] = [ 20, 30, 40];
+
+console.log(rest);
+// Expected output: A
